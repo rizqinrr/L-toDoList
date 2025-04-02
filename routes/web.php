a190e54b79fb6584e04b7d1bea9e7df02de38767
rizqinrr <studyrizqi@gmail.com>
2025-04-02 17:58:15 +0700

    commit 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Todo\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/todo', function () {
+//     return view('toDo.app');
+// });
+
+Route::get('/todo', [TodoController::class,'index']);
